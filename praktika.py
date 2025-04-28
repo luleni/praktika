d34283e9069651e53aaf4fa2247ded39f03fb910
luleni <lenapetrovkhyh@gmail.com>
2025-04-28 13:34:06 +0700

    third com

--- praktika.py
+++ praktika.py
@@ -1,9 +1,10 @@
-print('Введите J: ')
-J = str(input())
-print('Введите S: ')
-S = str(input())
-count = 0
-for i in S:
-    if i in J:
-        count += 1
-print('Вывод: ',count)
+print('Введите целочисленный массив nums: ')
+nums = [input() for i in range(6)]
+print(nums)
+def counter(nums):
+    for i in nums:
+        countt = nums.count(i)
+        if countt >=2:
+            return True
+        return False
+print(counter(nums))

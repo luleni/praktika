08791c82253c0741ebf4976ff76d56b8fa6e3ab5
luleni <lenapetrovkhyh@gmail.com>
2025-04-29 16:49:57 +0700

    2.3

--- praktika.py
+++ praktika.py
@@ -1,20 +1,25 @@
-class Train:
-    def __init__(self,destination,numtrain,departuretime):
-        self.destination = destination
-        self.numtrain = numtrain
-        self.departuretime = departuretime
-
+class Numbers:
+    def __init__(self,num1, num2):
+        self.num1 = num1
+        self.num2 = num2
     def output_info(self):
-        print("Название пункта назначения:",self.destination)
-        print("Номер поезда:", self.numtrain)
-        print("Время отправления:", self.departuretime)
+        print('Первое число:', self.num1)
+        print(f'Второе число:{self.num2}\n')
+    def change(self, new_num1, new_num2):
+        self.num1 = new_num1
+        self.num2 = new_num2
+    def sum(self):
+        return self.num1 + self.num2
+    def max(self):
+        return max(self.num1, self.num2)
+numbers = Numbers(3,5)
+numbers.output_info()
+numbers.change(2,4)
+numbers.output_info()
+print('Сумма чисел:', numbers.sum())
+print('Наибольшее число:', numbers.max())
 
-trains = []
-trains.append(Train("Москва", 123, "13:00"))
-trains.append(Train("Санкт-Петербург", 456, "16:30"))
-trains.append(Train("Казань", 789, "19:30"))
 
-train_number = int(input("Введите номер поезда: "))
-for train in trains:
-    if train.numtrain == train_number:
-        train.output_info()
+
+
+

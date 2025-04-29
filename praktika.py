152d1cb56c72c92800a3767fd08593b960977d6e
luleni <lenapetrovkhyh@gmail.com>
2025-04-29 15:16:23 +0700

    Update praktika.py

--- praktika.py
+++ praktika.py
@@ -14,7 +14,10 @@ trains.append(Train("Москва", 123, "13:00"))
 trains.append(Train("Санкт-Петербург", 456, "16:30"))
 trains.append(Train("Казань", 789, "19:30"))
 
-train_number = int(input("Введите номер поезда: "))
-for train in trains:
-    if train.numtrain == train_number:
-        train.output_info()
+def find():
+    for train in trains:
+        if train.numtrain == train_number:
+            train.output_info()
+            return "Информация найдена"
+    return "Поезд не найден"
+print(find())

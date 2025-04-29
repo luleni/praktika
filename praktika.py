054736a51fc4bee1e3d80b3887daf2cee959c45d
luleni <lenapetrovkhyh@gmail.com>
2025-04-29 12:30:57 +0700

    2.2

--- praktika.py
+++ praktika.py
@@ -1,25 +1,20 @@
-class Student:
-    def __init__(self,surname,birthday,numgroup,progress):
-        self.surname = surname
-        self.birthday = birthday
-        self.numgroup = numgroup
-        self.progress = progress
-
-    def change_surname(self, new_surname):
-        self.surname = new_surname
-    def change_birthday(self, new_birthday):
-        self.birthday = new_birthday
-    def change_numgroup(self, new_numgroup):
-        self.numgroup = new_numgroup
+class Train:
+    def __init__(self,destination,numtrain,departuretime):
+        self.destination = destination
+        self.numtrain = numtrain
+        self.departuretime = departuretime
 
     def output_info(self):
-        conclusion = (f"Фамилия: {self.surname}\tДата рождения: {self.birthday}\n"
-                      f"Номер группы: {self.numgroup}\tУспеваемость: {self.progress}")
-        print(conclusion)
+        print("Название пункта назначения:",self.destination)
+        print("Номер поезда:", self.numtrain)
+        print("Время отправления:", self.departuretime)
 
-student1 = Student("Котов", "04.04.2004", "632", [5, 4, 3, 5, 4])
-student2 = Student("Кошечкина", "03.03.2003", "631", [4, 5, 4, 4, 3])
+trains = []
+trains.append(Train("Москва", 123, "13:00"))
+trains.append(Train("Санкт-Петербург", 456, "16:30"))
+trains.append(Train("Казань", 789, "19:30"))
 
-student1.change_numgroup("432")
-student1.output_info()
-student2.output_info()
+train_number = int(input("Введите номер поезда: "))
+for train in trains:
+    if train.numtrain == train_number:
+        train.output_info()

4c7681b8147fccf17bff534b7b67eeb31e70e06f
luleni <lenapetrovkhyh@gmail.com>
2025-04-30 11:31:35 +0700

    3.1

--- praktika.py
+++ praktika.py
@@ -1,20 +1,19 @@
-class Class:
-    def __init__(self, value1 = 0, value2 = 0):
-        if value1 == 0 and value2 == 0:
-            self.value1 = value1
-            self.value2 = value2
-            print("--Объект создан со значениями по умолчанию--")
-        else:
-            self.value1 = value1
-            self.value2 = value2
-            print("--Объект создан с параметрами--")
-    def __del__(self):
-        print("Объект удален =(")
-
-obj1 = Class()
-print("первое значение по умолчанию:", obj1.value1)
-print(f"второе значение по умолчанию: {obj1.value2}\n")
-
-obj2 = Class(456, 123)
-print("первое значение с параметрами:", obj2.value1)
-print(f"второе значение с параметрами: {obj2.value2}\n")
+class Worker:
+    def __init__(self,name,surname,rate,days):
+        self.name = name
+        self.surname = surname
+        self.rate = int(rate)
+        self.days = int(days)
+    def GetSalary(self):
+        salary = self.rate * self.days
+        return salary
+    def output_info(self):
+        print('Имя:',self.name)
+        print('Фамилия:', self.surname)
+        print('Ставка:', self.rate)
+        print('Кол-во отработанных дней:', self.days)
+        print(f'Зарплата: {self.GetSalary()}\n')
+worker1 = Worker("Кот", "Котов", "100", "5")
+worker1.output_info()
+worker2 = Worker("Кошечка", "Кошечкина", "200", "10")
+worker2.output_info()

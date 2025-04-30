f499054cdbdfefe62dad5924feca2e7fa7c367cf
luleni <lenapetrovkhyh@gmail.com>
2025-04-30 09:10:39 +0700

    2.4

--- praktika.py
+++ praktika.py
@@ -1,20 +1,15 @@
-class Numbers:
-    def __init__(self,num1, num2):
-        self.num1 = num1
-        self.num2 = num2
+class Number:
+    def __init__(self, value = 0):
+        self.value = value
+    def increase(self):
+        self.value += 1
+    def decrease(self):
+        self.value -= 1
     def output_info(self):
-        print('Первое число:', self.num1)
-        print(f'Второе число:{self.num2}\n')
-    def change(self, new_num1, new_num2):
-        self.num1 = new_num1
-        self.num2 = new_num2
-    def sum(self):
-        return self.num1 + self.num2
-    def max(self):
-        return max(self.num1, self.num2)
-numbers = Numbers(3,5)
-numbers.output_info()
-numbers.change(10,4)
-numbers.output_info()
-print('Сумма чисел:', numbers.sum())
-print('Наибольшее число:', numbers.max())
+        return self.value
+counter = Number()
+print(f"Начальное значение: {counter.output_info()}")
+counter.increase()
+print(f"Значение после увеличения: {counter.output_info()}")
+counter.decrease()
+print(f"Значение после уменьшения: {counter.output_info()}")

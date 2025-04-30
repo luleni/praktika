f74a54eaf91b3d61a4a0e265e539b7d28b9ec2cc
luleni <lenapetrovkhyh@gmail.com>
2025-04-30 10:53:03 +0700

    2.5

--- praktika.py
+++ praktika.py
@@ -1,15 +1,20 @@
-class Number:
-    def __init__(self, value = 0):
-        self.value = value
-    def increase(self):
-        self.value += 1
-    def decrease(self):
-        self.value -= 1
-    def output_info(self):
-        return self.value
-counter = Number()
-print(f"Начальное значение: {counter.output_info()}")
-counter.increase()
-print(f"Значение после увеличения: {counter.output_info()}")
-counter.decrease()
-print(f"Значение после уменьшения: {counter.output_info()}")
+class Class:
+    def __init__(self, value1 = 0, value2 = 0):
+        if value1 == 0 and value2 == 0:
+            self.value1 = value1
+            self.value2 = value2
+            print("Объект создан со значениями по умолчанию:")
+        else:
+            self.value1 = value1
+            self.value2 = value2
+            print("Объект создан с параметрами:")
+    def __del__(self):
+        print("Объект удален =(")
+
+obj1 = Class()
+print("первое значение по умолчанию:", obj1.value1)
+print(f"второе значение по умолчанию: {obj1.value2}\n")
+
+obj2 = Class(456, 123)
+print("первое значение с параметрами:", obj2.value1)
+print(f"второе значение с параметрами: {obj2.value2}\n")

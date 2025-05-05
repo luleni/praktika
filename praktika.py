7f2cb51063ba39646b81742d5a19b199ccda2765
luleni <lenapetrovkhyh@gmail.com>
2025-05-05 10:59:16 +0700

    3.2

--- praktika.py
+++ praktika.py
@@ -1,17 +1,25 @@
 class Worker:
     def __init__(self,name,surname,rate,days):
-        self.name = name
-        self.surname = surname
-        self.rate = int(rate)
-        self.days = int(days)
+        self.__name = name
+        self.__surname = surname
+        self.__rate = int(rate)
+        self.__days = int(days)
+    def name_get(self):
+        return self.__name
+    def surname_get(self):
+        return self.__surname
+    def rate_get(self):
+        return self.__rate
+    def days_get(self):
+        return self.__days
     def GetSalary(self):
-        salary = self.rate * self.days
+        salary = self.__rate * self.__days
         return salary
     def output_info(self):
-        print('Имя:',self.name)
-        print('Фамилия:', self.surname)
-        print('Ставка:', self.rate)
-        print('Кол-во отработанных дней:', self.days)
+        print('Имя:',self.name_get())
+        print('Фамилия:', self.surname_get())
+        print('Ставка:', self.rate_get())
+        print('Кол-во отработанных дней:', self.days_get())
         print(f'Зарплата: {self.GetSalary()}\n')
 worker1 = Worker("Кот", "Котов", "100", "5")
 worker1.output_info()

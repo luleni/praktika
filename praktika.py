c29a993311c48c0b8b3a60ec28ea4788a889d35a
luleni <lenapetrovkhyh@gmail.com>
2025-05-05 11:40:41 +0700

    3.3

--- praktika.py
+++ praktika.py
@@ -1,27 +1,27 @@
-class Worker:
-    def __init__(self,name,surname,rate,days):
-        self.__name = name
-        self.__surname = surname
-        self.__rate = int(rate)
-        self.__days = int(days)
-    def name_get(self):
-        return self.__name
-    def surname_get(self):
-        return self.__surname
-    def rate_get(self):
-        return self.__rate
-    def days_get(self):
-        return self.__days
-    def GetSalary(self):
-        salary = self.__rate * self.__days
-        return salary
-    def output_info(self):
-        print('Имя:',self.name_get())
-        print('Фамилия:', self.surname_get())
-        print('Ставка:', self.rate_get())
-        print('Кол-во отработанных дней:', self.days_get())
-        print(f'Зарплата: {self.GetSalary()}\n')
-worker1 = Worker("Кот", "Котов", "100", "5")
-worker1.output_info()
-worker2 = Worker("Кошечка", "Кошечкина", "200", "10")
-worker2.output_info()
+class Calculation:
+    def __init__(self):
+        self.calculationLine = ""
+    def SetCalculation(self,newLine):
+        self.calculationLine = str(newLine)
+        return newLine
+    def SetLastSymbolCalculationLine(self, symbol):
+        self.calculationLine += str(symbol)
+    def GetCalculationLine(self):
+        return self.calculationLine
+    def GetLastSymbol(self):
+        return self.calculationLine[-1]
+    def DeleteLastSymbol(self):
+        self.calculationLine = self.calculationLine[:-1]
+        return self.calculationLine
+calc = Calculation()
+print('Начальная строка:',
+      calc.GetCalculationLine())
+print('Строка после изменения:',
+      calc.SetCalculation(12345))
+calc.SetLastSymbolCalculationLine(6)
+print('Последний символ:',
+      calc.GetLastSymbol())
+print('Строка после удаления последнего символа:',
+      calc.DeleteLastSymbol())
+print('Строка после изменений:',
+      calc.GetCalculationLine())
